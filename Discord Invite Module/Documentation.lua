95c4e4293c01704810aac0441f98df1297edee3c
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2021-09-13 10:15:52 +0200

    Create Documentation.lua

---
+++ Discord Invite Module/Documentation.lua
@@ -0,0 +1,66 @@
+<table> Module {
+    <table> Data {
+        <string> ServerName,
+        <string> ServerIcon,
+        <string> InviteCode,
+    },
+    <table> Config {
+        <string> Filename,
+    },
+    <table> Connections {
+        <RBXScriptSignal> Join,
+        <RBXScriptSignal> NoThanks,
+    },  
+    <table> Tween {
+        <table> Durations {
+            <int> Popup,
+            <int> Fade,
+        },
+        <Enum.EasingStyle> Style,
+    },
+    <boolean> IsPrompting,
+}
+
+----------
+
+<function> Module.Prompt(<table> {
+    <string> ServerName, -- [Required]
+    <string> ServerIcon, -- [Optional]
+    <string> InviteCode, -- [Required]
+}),
+--  [*] Prompts an invitation if not already prompting.
+
+----------
+
+<function> Module.Select(<boolean> yesNo),
+--  [*] Select the option to accept or ignore the invitation.
+
+----------
+
+<function> Module.JoinServer(<string> inviteCode),
+--  [*] Joins server without prompt, will still prompt on their Discord client.
+
+----------
+
+<function> Module.HidePrompt(),
+--  [*] Instantaneously hides the prompt, if one is prompting.
+
+----------
+
+<function> Module.GetCodeFromInvite(<string> invite),
+--  [*] Returns the invite code of invite.
+
+----------
+
+<function> Module.GetConfig(),
+--  [*] Returns the entire config.
+
+----------
+
+<function> Module.GetInviteData(<string> inviteCode),
+--  [*] Only returns the config attached to the invite code.
+}
+
+--------------------------------------------------
+
+return Module
